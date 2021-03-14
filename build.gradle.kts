75a61cb6d584cb70f3314d961a5d7f149c62e441
Wojtek Zieba <wojtek.zieba@automattic.com>
2021-03-14 16:46:33 +0100

    Put both plugins in pluginManagement

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
-        classpath("com.squareup.sqldelight:gradle-plugin:1.4.4")
+//        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
+//        classpath("com.squareup.sqldelight:gradle-plugin:1.4.4")
     }
 }
 
