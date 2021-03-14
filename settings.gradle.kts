e20123c4c53b9ee613c0d6a3b075ced20880f590
Wojtek Zieba <wojtek.zieba@automattic.com>
2021-03-14 16:48:03 +0100

    Put only SQLDelight plugin in buildscript

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,6 @@ pluginManagement {
     plugins {
         kotlin("multiplatform") version "1.4.31"
         kotlin("native.cocoapods") version "1.4.31"
-        id("com.squareup.sqldelight") version "1.4.4"
     }
     repositories {
         google()
